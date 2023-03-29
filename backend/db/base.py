2168661bf7aee451810ebe8887caed5fcdaffbdc
Ocalak <96614838+Ocalak@users.noreply.github.com>
2023-03-29 14:08:53 +0200

    Delete backend directory

--- backend/db/base.py
+++
@@ -1,4 +0,0 @@
-#db > base.py
-from db.base_class import Base
-from db.models.flats import Flat 
-from db.models.users import User
--- backend/schemas/users.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-
-#properties required during user creation
-class UserCreate(BaseModel):
-    username: str
-    email : str
-    password : str
